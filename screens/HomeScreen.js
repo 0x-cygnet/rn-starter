184557108cb982723dde593b948d9f836b11eacc
p00gz <bloopsocket@gmail.com>
2021-04-30 14:23:59 -0500

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>HomeScreen</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
+
+export default HomeScreen;
