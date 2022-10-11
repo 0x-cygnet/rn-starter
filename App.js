488d25dc5920a39097e0731d91e9a7782768eee2
0x-cygnet <bob@cygnetops.com>
2022-10-11 09:47:16 -0500

    fix src

---
+++ App.js
@@ -0,0 +1,17 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
