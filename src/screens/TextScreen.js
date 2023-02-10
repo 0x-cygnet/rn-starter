a90c802ad2ee717855e089b0762b3f2ecb94a837
Akshay Kumar <akshaychaudhary55@gmail.com>
2023-02-10 10:52:40 +0530

    added text input value

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,30 @@
-import React from "react";
-import { Text ,View , StyleSheet} from "react-native";
+import React, { useState } from "react";
+import {Text, View , StyleSheet,TextInput} from "react-native";
 
 const TextScreen = () => {
+  const [name, setName] = useState('');
     return (
         <View>
+        <Text>Enter Name:</Text>
+       <TextInput 
+       style={styles.input}
+       autoCapitalize="none"
+       autoCorrect={false}
+       value={name}
+       onChangeText={newValue => setName(newValue)}
+       />
+      <Text>My Name: {name}</Text>
 
-      <Text> sdfsdf!!</Text>
       </View>
      );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+});
 
 export default TextScreen;
