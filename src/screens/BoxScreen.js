bd9e8dcd19b943ff54fc03eb2b0e090e2ef38b16
Akshay Kumar <akshaychaudhary55@gmail.com>
2023-02-15 10:54:39 +0530

    play with flrx properties

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,12 +2,28 @@ import React from "react";
 import { Text,View,StyleSheet } from "react-native";
 
 const BoxScreen = () => {
- return <View>
-    <Text> Welcome to Box screen layout !!</Text>
+ return <View style={styles.viewStyle}>
+    <Text style={styles.textStyle}> Child #1 </Text>
+    <Text style={styles.textStyle}> Child #2 </Text>
+    <Text style={styles.textStyle}> Child #3</Text>
+
  </View>
 };
 
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+ viewStyle: {
+   borderWidth: 5,
+   borderColor:'black',
+   height:200,
+   justifyContent:'space-evenly'
+
+ },
+ textStyle: {
+   borderWidth:1,
+   borderColor:'red'
+
+ }
+});
 
 export default BoxScreen;
